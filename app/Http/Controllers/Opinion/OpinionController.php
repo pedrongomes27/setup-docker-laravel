ee354f533019474f7362df9e47f8f240f2c3009a
pedrongomes27 <pedro.ngomes27@hotmail.com>
2023-05-03 08:13:20 -0300

    Migrations, Models e Controllers

---
+++ app/Http/Controllers/Opinion/OpinionController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers\Opinion;
+
+class OpinionController {
+    public function index(){
+        return view('Opinion/index');
+    }
+}
---
+++ resources/views/Admin/Feedback/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pesquisa de Satisfação</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <div id="app" class="app">
+        <div class="wave-1">
+            <div class="wave-2">
+                <div class="wave-3">
+                    <h1>Pesquisa de Satisfação</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
